62af503d41b5c3a4cc80951feef0f61e0babb142
Paul Fry <paul-fry@hotmail.com>
2022-11-08 14:20:36 +1100

    filled out data type generation use cases (see py/dt_data_generation.py)

--- py/dt_data_generation.py
+++ py/dt_data_generation.py
@@ -1,18 +1,16 @@
 import logging
 import random
 import string
+from datetime import date
 from datetime import datetime
 from datetime import timedelta
 
 from faker import Faker
 
+current_dt_obj = datetime.now()
+
 # create and initialize a faker generator. Ireland localisation arg provided
 fake_generator = Faker('en_IE')
-
-current_dt_obj = datetime.now()
-# Can use 'current_dt_obj' to get other date parts. E.g. 'current_dt_obj.year'
-current_date_str = current_dt_obj.strftime('%d-%m-%Y')
-current_time_str = current_dt_obj.strftime('%H:%M:%S')
 
 # Set up a specific logger with our desired output level"""
 logging.basicConfig(format='%(message)s')
@@ -26,6 +24,12 @@ def gen_fake_string_data(row):
 
     if row['col_name'].upper() == 'FIRST_NAME':
         fake_string_data = fake_generator.first_name()
+    # to facilitate table joins, limit IDs to a range of 10 values
+    elif 'ID' in row['col_name'].upper():
+        fake_string_data = str(random.randint(1, 10))
+    # in case a date field is accidently captured as a string, include data generation for it here
+    elif 'DATE' in row['col_name'].upper():
+        fake_string_data = f"to_date('{fake_generator.date_between(current_dt_obj - timedelta(days=5), current_dt_obj)}')"
 
     elif row['data_type'].startswith('VARCHAR'):
         # generating a string to max length produces too long strings. Limit to 20 and uncomment the line below if otherwise wanted.
@@ -47,8 +51,21 @@ def gen_fake_numeric_data(row):
         precision = split_str[0]
         logger.debug(f'precision = {precision}')
 
-        # generating data to 38 precision produces too high numbers. Limit to 50 and uncomment the line below if otherwise wanted.
-        fake_numeric_data = random.randint(1, 50)
+        # if it's a 'quantity' field, limit to values 1-10
+        if 'QUANTITY' in row['col_name'].upper():
+            fake_numeric_data = random.randint(1, 10)
+        # to facilitate table joins, limit IDs to a range of 10 values
+        elif 'ID' in row['col_name'].upper():
+            fake_numeric_data = random.randint(1, 10)
+        # limit price to 100
+        elif 'PRICE' in row['col_name'].upper():
+            fake_numeric_data = str(f"'{random.randint(1, 100)}.00'")
+        # limit year a value in the last 5 years
+        elif 'YEAR' in row['col_name'].upper():
+            fake_numeric_data = random.randint(date.today().year-5, date.today().year)
+        else:
+            # generating data to high precisions (e.g., 38) too big numbers. Limit to 50 and uncomment the line below if otherwise wanted.
+            fake_numeric_data = random.randint(1, 50)
 
         # though if you do want to generate fake data to precision, use the below:
         # generate random number to (max) precision
